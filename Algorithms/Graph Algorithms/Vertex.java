4fbac2dd27c03065e60e1da7d8a2d0c4b6d0bfcc
Minyoung Cho <116537478+minyoungcho99@users.noreply.github.com>
2023-05-09 01:24:20 -0400

    Update Vertex.java

--- Algorithms/Graph Algorithms/Vertex.java
+++ Algorithms/Graph Algorithms/Vertex.java
@@ -1,21 +1,7 @@
-/**
- * Class representing a vertex.
- *
- * DO NOT EDIT THIS CLASS!!!
- *
- * @author CS 1332 TAs
- * @version 1.0
- */
 public class Vertex<T> {
 
     private T data;
-
-    /**
-     * Creates a Vertex object holding the given data.
-     *
-     * @param data the object that is stored in this Vertex
-     * @throws IllegalArgumentException if data is null
-     */
+    
     public Vertex(T data) {
         if (data == null) {
             throw new IllegalArgumentException("Data cannot be null.");
@@ -36,12 +22,7 @@ public class Vertex<T> {
     public int hashCode() {
         return data.hashCode();
     }
-
-    /**
-     * Gets the data in this vertex.
-     *
-     * @return the data in this vertex
-     */
+    
     public T getData() {
         return data;
     }
